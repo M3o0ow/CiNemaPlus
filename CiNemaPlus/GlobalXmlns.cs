89f39f82b3b39dc9757bbf986c8a11630b0a6802
M3o0ow <MrGamer15200@gmail.com>
2026-05-07 09:14:00 -0300

    Init the project

---
+++ CiNemaPlus/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CiNemaPlus")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CiNemaPlus.Pages")]
